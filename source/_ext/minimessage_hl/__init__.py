4a2f10827374580ece8d0ee203d549c22ccfcb4b
zml <zml@stellardrift.ca>
2022-02-12 18:58:51 -0800

    license headers, a bit of cleanup

--- source/_ext/minimessage_hl/__init__.py
+++ source/_ext/minimessage_hl/__init__.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
+# This file is part of adventure-docs, licensed under the MIT License.
+#
+# Copyright (c) 2017-2022 KyoriPowered
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from sphinx.application import Sphinx
 import pygments.lexers
 import pygments.lexers._mapping
-from docutils import nodes
-from docutils.parsers.rst.roles import code_role
 
 
 def setup(app: Sphinx):
@@ -12,14 +31,7 @@ def setup(app: Sphinx):
     pygments.lexers._mapping.LEXERS['minimessage'] = ('minimessage_hl.lexer', 'MiniMessage component', ('minimessage', 'mm'), ('*.mm',), ())
     pygments.lexers.find_lexer_class('MiniMessage component') # update the cache
 
-    #app.add_role('mm', _mm_role)
-
     # Register an interpreted text role for inline syntax highlighting
     return {
         'parallel_read_safe': True
     }
-
-def _mm_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
-    options['language'] = 'minimessage'
-    return code_role(name, rawtext, text, lineno, inliner, options, content)
-
