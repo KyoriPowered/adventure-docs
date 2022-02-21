f3582db317bb901ede1566ee9bac311bb3cb2e31
zml <zml@stellardrift.ca>
2022-02-20 16:20:16 -0800

    Work around CloudFlare's nonsense for GH Pages migration

--- source/_ext/adventure_docs_extensions/__init__.py
+++ source/_ext/adventure_docs_extensions/__init__.py
@@ -21,15 +21,20 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from pathlib import Path
 import re
 from sphinx.application import Sphinx
 from docutils.parsers.rst import nodes
 
+
 def setup(app: Sphinx):
     app.add_role("mojira", mojira_role)
+    app.connect('html-collect-pages', _fix_cloudflare_name_mangling)
+
 
 _issue_regex = re.compile(r'[A-Z]+-[1-9][0-9]*')
 _mojira_url = "https://bugs.mojang.com/browse/"
+
 
 def mojira_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
     if not _issue_regex.fullmatch(text):
@@ -38,7 +43,36 @@ def mojira_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
         return [prb], [msg]
 
     ref = _mojira_url + text
-    
+
     node = nodes.reference(rawtext, text, refuri=ref, **options)
 
     return [node], []
+
+
+def _fix_cloudflare_name_mangling(app: Sphinx):
+    """
+    Cloudflare Pages does mangling of page names to strip the `.html` from the end.
+    We need to preserve those links when moving to GitHub pages though, so we build out
+    redirects using directory names.
+
+    References https://gitlab.com/documatt/sphinx-reredirects/-/blob/master/sphinx_reredirects/__init__.py liberally
+    """
+
+    docs = app.env.found_docs
+    outdir = Path(app.outdir)
+
+    for doc in docs:
+        if doc.endswith("/index") or (doc + "/index" in docs):
+            continue
+
+        doc_basename = doc.split('/')[-1]
+
+        # Write out a temporary file
+        from_path = outdir / doc / "index.html"
+        to_uri = f"../{doc_basename}.html"
+        from_contents = f'<html><head><meta http-equiv="refresh" content="0; url={to_uri}"></head></html>'
+
+        from_path.parent.mkdir(parents=True, exist_ok=True)
+        from_path.write_text(from_contents, encoding='UTF-8')
+
+    return []
