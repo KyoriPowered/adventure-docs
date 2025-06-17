f46606bbf678db412af2778c27600caeac3b3c6f
zml <zml@stellardrift.ca>
2025-06-17 07:34:25 -0700

    adventure: 4.22.0

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,7 @@ author = 'KyoriPowered'
 # The short X.Y versions
 
 # The latest version of the Adventure api
-api_version = '4.21.0'
+api_version = '4.22.0'
 
 # The latest versions of adventure-platform builds
 platform_version = '4.4.0'
